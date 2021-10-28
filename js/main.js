cc0ef085e4a162f2e44694c97354992560230c6e
kazemi-nezhad <kazemimh95@gmail.com>
2021-10-28 23:48:19 +0330

    editing about section #3

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+}
